e3568516df183633fefe6b1ac6b83ec2192d6161
suba <145760609+suba6325@users.noreply.github.com>
2024-12-15 22:38:06 +0300

    Update task2.py

--- laba1/task2.py
+++ laba1/task2.py
@@ -1,9 +1,9 @@
-from task import Car, Book, BankAccount  # TODO: импортируйте классы, созданные в ходе выполнения прошлого задания
+from task_1 import Car, Book, BankAccount  # TODO: импортируйте классы, созданные в ходе выполнения прошлого задания
 
 if __name__ == "__main__":
     book = Book("Under the Dome", "Stephen King", 1024, "фантастика")
     car = Car("Toyota", "Supra A90", 2020, "Red")
-    BA = BankAccount(1233113, "Iv KT", 1000)
+    account = BankAccount(12456, "Ivan Ivanov", 1000)
 
     try:
         book.remaining_pages("20")
@@ -16,6 +16,6 @@ if __name__ == "__main__":
         print('Ошибка: неправильные данные')
 
     try:
-            BA.payment(10000)
+        account.payment(10000)
     except ValueError:
         print('Ошибка: неправильные данные')
